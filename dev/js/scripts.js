313057af2893403f77f6584de2eb74d19214fdb4
Josh <joshGdovindesigns.com>
2025-05-18 20:51:08 -0400

    compete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,3 +5,18 @@ Fancybox.bind("[data-fancybox]", {
   // Your custom options
 });
 
+let banner = document.querySelector("#blue-bar");
+let closeBannerBtn = document.querySelector("#close-bar-btn");
+
+// Check local storage on page load
+if (localStorage.getItem('bannerClosed') === 'true') {
+    banner.style.display = 'none'; // Hide banner if closed before
+}
+
+closeBannerBtn.addEventListener("click", hideBlueBar);
+
+function hideBlueBar(){
+   banner.style.display = 'none'; // Hide banner
+   localStorage.setItem('bannerClosed', 'true'); // Store in local storage
+}
+
